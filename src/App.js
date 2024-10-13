640a47e890e262001ae5923bb6b5086c9290a2ba
mathanlakshmanan <mathankumarmca92@gmail.com>
2024-10-13 11:28:07 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import Coordinate from './assets/coordinate.png';
 import Humidity from './assets/humidity.png';
 import Visibility from './assets/visibility.png';
 import Windy from './assets/windy.png';
+import Pressure from './assets/pressure.png';
+import Sun from './assets/sun.png';
 import { Circles } from 'react-loader-spinner';
 
 function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState([]);
   const [loader, setLoader] = useState(false);
+  const [sunrise, setSunrise] = useState();
+  const [sunset, setSunset] = useState();
 
   useEffect(() => {
-    document.body.style.backgroundImage = "url('bg.jpg')";
-  }, [weather]);
+    document.body.style.backgroundImage = "linear-gradient(327deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 50%, rgba(0,212,255,1) 100%)";    
+  }, []);
 
   const weatherFun = async () => {
     
@@ -33,9 +37,16 @@ function App() {
     });
 
     setWeather(result.data.weather);
-
+    console.log("mathan", result.data.weather);
+    
     if(result){
       setLoader(false);
+      const sunrisedata = new Date(parseInt(result.data.weather.sys.sunrise+'000'));
+      let sunrise = sunrisedata.toLocaleTimeString();
+      setSunrise(sunrise);
+      const sunsetdata = new Date(parseInt(result.data.weather.sys.sunset+'000'));
+      let sunset = sunsetdata.toLocaleTimeString();
+      setSunset(sunset);
     }
   };
 
@@ -44,7 +55,7 @@ function App() {
       <div className="bg-dark p-5 text-white">
         <h1 className="mb-3 text-center">Weather Checker</h1>
         <div className="row justify-content-around mb-5">
-          <div className="w-50 mobileInput">
+          <div className="w-50 mobileInput mb-3">
             <input
               type="text"
               className="form-control"
@@ -53,7 +64,7 @@ function App() {
               alt="weatherInput"
               placeholder="Enter The Your City"
             />
-            <p id="error"></p>
+            <div id="error"></div>
           </div>
           <div className="text-center">
             <input
@@ -79,51 +90,88 @@ function App() {
         </div>
         : 
         Object.keys(weather).length > 0 ? <>
-        <div className="text-center">
-          <div className="card-border">
-            <div className="mb-2">
-              <img src={CityImg} className="h-150 w-25" alt="city" />
-              <p className="">City</p>
-            </div>
-            <div className="">
-              <p>{weather?.name} </p>
-            </div>
-          </div>
-        </div>
 
-        <hr />
-
-        <div className="row">
-          
-            {weather?.weather?.map((data) => {
+<div className='row'>
+{weather?.weather?.map((data) => {
               return (
                 <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <div key={data?.id} className="card-border">
                   <div className="">
                     <img
                       src={`https://openweathermap.org/img/wn/${data?.icon}@2x.png`}
-                      className="h-150 w-100"
+                      className="h-100 w-100"
                       alt='weatherimage'
                     />
                   </div>
                   <div className="align-content-center">
-                    <p>{data?.main}</p>
-                    <p>{data?.description}</p>
+                    <div>{data?.main}</div>
+                    <div>{data?.description}</div>
+                    <h4>{((weather.main.temp - 32) * 5/9).toFixed(0)}℃</h4>
                   </div>
                 </div>
                 </div>
               );
             })}
+        </div>
+
+        <hr />
+
+        <div className="row">
+          
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+          <div className="card-border">
+            <div className="">
+              <img src={CityImg} className="h-100 w-100" alt="city" />
+              <div className="mt-1">City</div>
+            </div>
+            <div className="">
+              <div>{weather?.name} </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div className="card-border">
+              <div className="mb-2">
+                <img
+                  src={Sun}
+                  className="h-100 w-100"
+                  alt="windy"
+                />
+                <div className="">Sunrise</div>
+              </div>
+              <div className="">
+                <div>{sunrise}</div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div className="card-border">
+              <div className="mb-2">
+                <img
+                  src={Sun}
+                  className="h-100 w-100"
+                  alt="windy"
+                />
+                <div className="">Sunset</div>
+              </div>
+              <div className="">
+                <div>{sunset}</div>
+              </div>
+            </div>
+          </div>
           
           <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
             <div className="card-border">
               <div className="mb-2">
                 <img src={Windy} className="h-100 w-100" alt="windy" />
-                <p className="">Windy</p>
+                <div className="">Windy</div>
               </div>
               <div className="">
-                <p>Speed: {weather?.wind?.speed} km/h</p>
-                <p>degree: {weather?.wind?.deg} ℃</p>
+                <div>Speed: {weather?.wind?.speed} km/h</div>
+                <div>degree: {weather?.wind?.deg} ℃</div>
               </div>
             </div>
           </div>
@@ -131,10 +179,10 @@ function App() {
             <div className="card-border">
               <div className="mb-2">
                 <img src={Cloudy} className="h-100 w-100" alt="windy" />
-                <p className="">Clouds</p>
+                <div className="">Clouds</div>
               </div>
               <div className="">
-                <p>Cloud Cover: {weather?.clouds?.all} %</p>
+                <div>Cloud Cover: {weather?.clouds?.all} %</div>
               </div>
             </div>
           </div>
@@ -146,10 +194,10 @@ function App() {
                   className="h-100 w-100"
                   alt="windy"
                 />
-                <p className="">Humidity</p>
+                <div className="">Humidity</div>
               </div>
               <div className="">
-                <p>Humidity: {weather?.main?.humidity} %</p>
+                <div>Humidity: {weather?.main?.humidity} %</div>
               </div>
             </div>
           </div>
@@ -161,11 +209,11 @@ function App() {
                   className="h-100 w-100"
                   alt="windy"
                 />
-                <p className="">Coordinate</p>
+                <div className="">Coordinate</div>
               </div>
               <div className="">
-                <p>Latitude: {weather?.coord?.lat} </p>
-                <p>Longitude: {weather?.coord?.lon} </p>
+                <div>Latitude: {weather?.coord?.lat} </div>
+                <div>Longitude: {weather?.coord?.lon} </div>
               </div>
             </div>
           </div>
@@ -174,16 +222,34 @@ function App() {
               <div className="mb-2">
                 <img
                   src={Visibility}
-                  className="h-150 w-100"
+                  className="h-100 w-100"
                   alt="windy"
                 />
-                <p className="">Visibility</p>
+                <div className="">Visibility</div>
               </div>
               <div className="">
-                <p>{weather?.visibility / 1000} KM</p>
+                <div>{weather?.visibility / 1000} KM</div>
               </div>
             </div>
           </div>
+
+          <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div className="card-border">
+              <div className="mb-2">
+                <img
+                  src={Pressure}
+                  className="h-100 w-100"
+                  alt="windy"
+                />
+                <div className="">Pressure</div>
+              </div>
+              <div className="">
+                <div>{weather?.main?.pressure} hPa</div>
+              </div>
+            </div>
+          </div>
+
+          
         </div> </>: ""}
       </div>
     </>
